3b65fe9b7d1655ad7f3e9a9d85f3c25e97aab440
Benji Altman <baltman@sabgrp.com>
2020-12-04 00:22:14 -0600

    readme and gitignore

---
+++ AsyncThen/AsyncExtensions.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncThen
+{
+	/// <summary>
+	/// Extension methods for tasks
+	/// </summary>
+	public static class AsyncExtensions
+	{
+		//TODO add XML comments
+		public static Task<TOutput> Then<TInput, TOutput>(this Task<TInput> task, Func<TInput, TOutput> func)
+		{
+			var tcs = new TaskCompletionSource<TOutput>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+					tcs.TrySetResult(func(t.Result));
+			});
+			return tcs.Task;
+		}
+		public static Task<TOutput> Then<TOutput>(this Task task, Func<TOutput> func)
+		{
+			var tcs = new TaskCompletionSource<TOutput>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+					tcs.TrySetResult(func());
+			});
+			return tcs.Task;
+		}
+		public static Task Then<TInput>(this Task<TInput> task, Action<TInput> func)
+		{
+			var tcs = new TaskCompletionSource<bool>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+				{
+					func(t.Result);
+					tcs.TrySetResult(true);
+				}
+			});
+			return tcs.Task;
+		}
+		public static Task Then(this Task task, Action func)
+		{
+			var tcs = new TaskCompletionSource<bool>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+				{
+					func();
+					tcs.TrySetResult(true);
+				}
+			});
+			return tcs.Task;
+		}
+		public static Task<TOutput> Then<TInput, TOutput>(this Task<TInput> task, Func<TInput, Task<TOutput>> func)
+		{
+			var tcs = new TaskCompletionSource<TOutput>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+				{
+					var resultingTask = func(t.Result);
+					resultingTask.ContinueWith(u =>
+					{
+						if (u.IsFaulted)
+							tcs.TrySetException(u.Exception.InnerExceptions);
+						else if (u.IsCanceled)
+							tcs.TrySetCanceled();
+						else
+							tcs.TrySetResult(resultingTask.Result);
+					});
+				}
+			});
+			return tcs.Task;
+		}
+		public static Task<TOutput> Then<TOutput>(this Task task, Func<Task<TOutput>> func)
+		{
+			var tcs = new TaskCompletionSource<TOutput>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+				{
+					var resultingTask = func();
+					resultingTask.ContinueWith(u =>
+					{
+						if (u.IsFaulted)
+							tcs.TrySetException(u.Exception.InnerExceptions);
+						else if (u.IsCanceled)
+							tcs.TrySetCanceled();
+						else
+							tcs.TrySetResult(resultingTask.Result);
+					});
+				}
+			});
+			return tcs.Task;
+		}
+		public static Task Then<TInput>(this Task<TInput> task, Func<TInput, Task> func)
+		{
+			var tcs = new TaskCompletionSource<bool>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+				{
+					var resultingTask = func(t.Result);
+					resultingTask.ContinueWith(u =>
+					{
+						if (u.IsFaulted)
+							tcs.TrySetException(u.Exception.InnerExceptions);
+						else if (u.IsCanceled)
+							tcs.TrySetCanceled();
+						else
+							tcs.TrySetResult(true);
+					});
+				}
+			});
+			return tcs.Task;
+		}
+		public static Task Then(this Task task, Func<Task> func)
+		{
+			var tcs = new TaskCompletionSource<bool>();
+			task.ContinueWith(t =>
+			{
+				if (t.IsFaulted)
+					tcs.TrySetException(t.Exception.InnerExceptions);
+				else if (t.IsCanceled)
+					tcs.TrySetCanceled();
+				else
+				{
+					var resultingTask = func();
+					resultingTask.ContinueWith(u =>
+					{
+						if (u.IsFaulted)
+							tcs.TrySetException(u.Exception.InnerExceptions);
+						else if (u.IsCanceled)
+							tcs.TrySetCanceled();
+						else
+							tcs.TrySetResult(true);
+					});
+				}
+			});
+			return tcs.Task;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # AsyncThen
+
+Extension methods for C#'s asynchronous Tasks.
+
+
+##Usage:
+Include `using namespace AsyncThen;` at the top of your C# file.
+
+Supported methods:
+- `Task<B> Task<A>.Then(Func<A, B>)`
+- `Task Task<A>.Then(Action<A>)`
+- `Task<B> Task.Then(Func<B>)`
+- `Task Task.Then(Action)`
+- `Task<B> Task<A>.Then(Func<A, Task<B>>)`
+- `Task Task<A>.Then(Func<A, Task>)`
+- `Task<B> Task.Then(Func<Task<B>>)`
+- `Task Task.Then(Func<Task>)`
